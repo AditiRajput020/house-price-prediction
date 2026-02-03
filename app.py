239a22e5b92d0f37f83ed12ff3bb1ad1ed8bd24a
AditiRajput020 <aditirajput6173@gmail.com>
2026-02-03 16:04:41 +0530

    Initial commit - House Price Prediction ML project

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+st.set_page_config(page_title="House Price Prediction", layout="centered")
+
+st.title("🏠 House Price Prediction System")
+st.write("Enter house details to predict the price")
+
+# Load pipeline
+pipeline = joblib.load("house_price_pipeline.pkl")
+
+# Inputs
+longitude = st.number_input("Longitude", value=-122.23)
+latitude = st.number_input("Latitude", value=37.88)
+housing_median_age = st.number_input("Housing Median Age", value=41.0)
+total_rooms = st.number_input("Total Rooms", value=880.0)
+total_bedrooms = st.number_input("Total Bedrooms", value=129.0)
+population = st.number_input("Population", value=322.0)
+households = st.number_input("Households", value=126.0)
+median_income = st.number_input("Median Income", value=8.3252)
+
+ocean_proximity = st.selectbox(
+    "Ocean Proximity",
+    ["<1H OCEAN", "INLAND", "ISLAND", "NEAR BAY", "NEAR OCEAN"]
+)
+
+if st.button("Predict House Price"):
+    input_df = pd.DataFrame([{
+        "longitude": longitude,
+        "latitude": latitude,
+        "housing_median_age": housing_median_age,
+        "total_rooms": total_rooms,
+        "total_bedrooms": total_bedrooms,
+        "population": population,
+        "households": households,
+        "median_income": median_income,
+        "ocean_proximity": ocean_proximity
+    }])
+
+    prediction = pipeline.predict(input_df)
+
+    st.success(f"💰 Predicted House Price: ${prediction[0]:,.2f}")
